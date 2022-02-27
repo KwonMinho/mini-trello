abe9cadd623c55033e758f0a80cd89e8351c677d
KwonMinho <alsgh458@gmail.com>
2022-02-27 12:25:03 +0900

    docs: ignore-favicon middlware 주석화

--- src/middleware/ignore-favicon.middleware.ts
+++ src/middleware/ignore-favicon.middleware.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 
 /**
  * @description: brower tap에서 http get 호출시, 자동으로 favicon.icon 요청하는 것에 대한 ignore 미들웨어
+ * @version 1.0.0
+ * @author minho(alsgh458-gmail)
+ * @see None
+ * @since 22.02.25
  */
 export const ignoreFaviconMiddleware = (
   req: Request,
