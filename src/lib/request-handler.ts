3c3c4763a903448d2a6613f6d7417923841bb64b
KwonMinho <alsgh458@gmail.com>
2022-02-27 12:46:59 +0900

    docs: request-handler 주석화

--- src/lib/request-handler.ts
+++ src/lib/request-handler.ts
@@ -1,5 +1,9 @@
 import Express from "express";
 
+/**
+ * @description: 원격 프로그램(핸들러)의 반환값을 json 형태로 변환하고 에러 발생시에 에러 처리 미들웨어로 흐름 제어하는 함수
+ * @param {Funtion} handler: 특정 path에 맵핑될 원격 프로그램(핸들러)
+ */
 export const wrap: Function =
   (handler: Function) =>
   async (
